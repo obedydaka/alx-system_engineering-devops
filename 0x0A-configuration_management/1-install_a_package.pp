1f0b48a191ba2cd7ed26064f89b5f7807f52ba02
Obedy Daka <obedydaka01@gmail.com>
2024-07-19 16:19:09 -0700

    pp2 fixed

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
-# This manifest installs a package
-exec { 'install_flask':
-  command => '/usr/bin/pip3 install flask==2.1.0',
+# This manifest installs Flask 2.1.0 and compatible Werkzeug version using pip3
+exec { 'install_flask_werkzeug':
+  command => '/usr/bin/pip3 install flask==2.1.0 werkzeug==2.1.1',
   path    => ['/usr/bin', '/bin'],
   unless  => '/usr/bin/pip3 show flask | grep -q "Version: 2.1.0"',
 }
