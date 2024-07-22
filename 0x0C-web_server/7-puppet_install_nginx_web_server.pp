730b5607e28dbb16a5349c267dc28668ef512c24
Obedy Daka <obedydaka01@gmail.com>
2024-07-22 15:52:55 -0700

    All pushed

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,1 +1,64 @@
-#!/usr/bin/env bash
+# Puppet manifest to install and configure Nginx
+
+# Ensure the package is installed
+package { 'nginx':
+  ensure => installed,
+}
+
+# Ensure the service is running and enabled
+service { 'nginx':
+  ensure     => running,
+  enable     => true,
+  subscribe  => File['/etc/nginx/sites-available/default'],
+}
+
+# Create the Hello World index.html file
+file { '/var/www/html/index.html':
+  ensure  => file,
+  content => 'Hello World!',
+  require => Package['nginx'],
+}
+
+# Configure the Nginx site
+file { '/etc/nginx/sites-available/default':
+  ensure  => file,
+  content => template('nginx/default.erb'),
+  require => Package['nginx'],
+}
+
+# Define the template for Nginx configuration
+file { '/etc/puppetlabs/code/environments/production/modules/nginx/templates/default.erb':
+  ensure  => file,
+  content => @("EOF")
+server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    
+    root /var/www/html;
+    index index.html index.htm index.nginx-debian.html;
+    
+    server_name _;
+    
+    location / {
+        try_files \$uri \$uri/ =404;
+    }
+    
+    location /redirect_me {
+        return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
+    }
+    
+    error_page 404 /custom_404.html;
+    location = /custom_404.html {
+        internal;
+    }
+}
+| EOF
+  require => Package['nginx'],
+}
+
+# Ensure the Nginx configuration is reloaded if the configuration file changes
+exec { 'reload-nginx':
+  command     => '/usr/sbin/nginx -s reload',
+  refreshonly => true,
+  subscribe   => File['/etc/nginx/sites-available/default'],
+}
